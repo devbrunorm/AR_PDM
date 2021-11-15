bfb45a903a3b26374f1ed1381ad708382bce04d2
devbrunorm <devbrunorm@gmail.com>
2021-11-15 18:37:39 -0300

    Correção nos radio buttons do Sexo e adicionado limite de caracteres nos textviews do dia de nascimento.

--- app/src/main/java/com/brunorm/ar_pdm/PersonalInfoActivity.java
+++ app/src/main/java/com/brunorm/ar_pdm/PersonalInfoActivity.java
@@ -1,8 +1,11 @@
 package com.brunorm.ar_pdm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class PersonalInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+
+        String[] genders = {"Masculino", "Feminino", "Outro"};
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.inputPersonalInfoGender);
+        for (int i = 0; i < 3; i++) {
+            String gender = genders[i];
+            RadioButton button = new RadioButton(this);
+            button.setText(gender);
+            button.setId(i);
+
+            radioGroup.addView(button);
+        }
+        radioGroup.setOrientation(LinearLayout.HORIZONTAL);
     }
 }
